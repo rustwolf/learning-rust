c9a3dcfec46da5a04ecafd91f7423504b561221d
rustwolf <azhar.mwm@gmail.com>
2022-07-27 03:57:28 +0530

    Added multi threading

--- src/lazystatic.rs
+++ src/lazystatic.rs
@@ -12,21 +12,34 @@ lazy_static! {
 #[automock]
 pub mod lazystatic {
     use std::sync::{Mutex, Arc};
+    use std::thread;
+    use std::time::Duration;
 
+    
     pub fn print_me() {
         // super::my_messages;
 
-        let my_message = super::my_messages.clone();
+        let handle = thread::spawn(|| {
+            for i in 1..10 {
+                println!("hi number {} from the spawned thread!", i);
+                let my_message = super::my_messages.clone();
 
-        my_message.lock().unwrap().push(String::from("Hi there we are here !!"));
+                my_message.lock().unwrap().push(String::from("Hi there we are here !!"));
+            }
+        });
+    
 
         // super::my_messages.lock().unwrap().push(String::from("Hello world Again "));
 
         // println!("{}", my_message.());
 
+        handle.join().unwrap();
+
+        let my_message = super::my_messages.clone();
 
         println!("{}", my_message.lock().unwrap().len());
 
         println!("Hello Broooo");
+
     }
 }
