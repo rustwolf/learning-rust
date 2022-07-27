c8de28e8d9d7da5fca2678aec275c08d164c9882
rustwolf <azhar.mwm@gmail.com>
2022-07-27 17:22:26 +0530

    Addes Thread with Atomic Ref Counter

--- src/thread.rs
+++ src/thread.rs
@@ -1,6 +1,33 @@
 pub mod thread {
+    use std::{sync::{Arc, Mutex}, thread};
+
 
     pub fn run_thread() {
         println!("This is from thread");
+
+        let a = Arc::new(Mutex::new(10));
+
+  
+            
+            // let mut all_threads = vec![];
+
+
+        for i in 1..10 {
+
+            let  a = a.clone();
+            let threads = thread::spawn(move || {
+                
+                *a.lock().unwrap()+=1;
+
+                println!("We are inside a thread , {}", *a.lock().unwrap());
+            });
+            // all_threads.push(threads);
+
+            threads.join().unwrap();
+        }
+
+
+        // threads.join().unwrap();
     }
 }
+
