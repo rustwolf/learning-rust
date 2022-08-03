7952a6708e46ff1c0abf332516805871852d7a37
rustwolf <azhar.mwm@gmail.com>
2022-08-03 17:58:30 +0530

    Some value to pointer

--- src/reference.rs
+++ src/reference.rs
@@ -1,5 +1,10 @@
 pub mod refereneces {
     pub fn main() {
         println!("WE are in references area");
+
+        let num = 10;
+        let ref_num = &num;
+
+        println!("{}", *ref_num);
     }
 }
