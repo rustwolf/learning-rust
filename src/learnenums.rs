ab776e51059c6738ee1837a05d26e0321b476189
rustwolf <azhar.mwm@gmail.com>
2022-08-27 04:29:41 +0530

    learnng enums printing value

--- src/learnenums.rs
+++ src/learnenums.rs
@@ -2,12 +2,16 @@ pub mod learnenums {
     #[derive(Debug)]
     pub enum Item {
         Something(String),
-        Nothing,
+        Nothing
     }
     pub fn main() {
         println!("We are learning Enums");
         let a = Item::Something(String::from("Hello World inside Enum"));
 
+        match &a {
+            Item::Something(a) => { println!("{}",a)},
+            Item::Nothing => { println!("WE found nothing")}
+        }
         println!("{:?}", a);
     }
 }
