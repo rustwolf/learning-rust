d207a72275445d2ba927e454b59ae6133a17abfa
rustwolf <azhar.mwm@gmail.com>
2022-10-01 19:27:46 +0530

    Created a ref cell with complex DT

--- src/refcell_learn.rs
+++ src/refcell_learn.rs
@@ -1,5 +1,30 @@
 pub mod refcell_learn {
-    pub fn main() {
-        println!("Learning refcell_learn");
+    use std::{cell::RefCell, rc::Rc};
+
+    #[derive(Debug)]
+    pub struct Person {
+        pub name : String
     }
+
+    pub fn main() {
+
+        let mut me = Person {
+            name : String::from("Azhar")
+        };
+
+        let ref_me = RefCell::new(me);
+
+        // println!("Learning refcell_learn {:?}", ref_me.into_inner());
+
+        change_name(ref_me.into_inner());
+
+        // let cell = RefCell::new(me);
+    }
+
+    pub fn change_name(mut person: Person) {
+        person.name = String::from("Khan");
+
+        println!("{}", person.name);
+    }
+
 }
